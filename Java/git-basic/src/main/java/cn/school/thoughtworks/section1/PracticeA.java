5d4997d4d876bed1c24595e9e92d77e21c7afa9f
Czkl <zw407461270@163.com>
2019-03-22 18:28:56 +0800

    Runtime environment

---
+++ Java/git-basic/src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -0,0 +1,16 @@
+package cn.school.thoughtworks.section1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PracticeA {
+    List<String> collectSameElements(List<String> collection1, List<String> collection2) {
+        List<String> result = new ArrayList<>();
+
+        System.out.println("collection1 = [" + collection1 + "], collection2 = [" + collection2 + "]");
+
+
+
+        return null;
+    }
+}
---
+++ Java/git-basic/src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -0,0 +1,12 @@
+package cn.school.thoughtworks.section3;
+
+import java.util.List;
+import java.util.Map;
+
+public class PracticeD {
+    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+
+
+        return null;
+    }
+}
